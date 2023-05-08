041cd1ed6a566879012de0b6d5dd34ce72a21e6b
Andy <68301335+Lancito01@users.noreply.github.com>
2023-05-08 20:57:30 -0300

    fixed typos

--- POO/actividad_clase_1.java
+++ POO/actividad_clase_1.java
@@ -5,34 +5,34 @@ public class actividad_clase_1{
         int edad = 2;
         double comida = 1.5;
         String sonido = "guau guau";
-        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+        System.out.println("Mi " + especie + " se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
 
         nombre = "Nemo";
         especie = "pez";
         edad = 1;
         comida = 0.14;
         sonido = "glu glu";
-        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+        System.out.println("Mi " + especie + " se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
 
         nombre = "Silvestre";
         especie = "gato";
         edad = 3;
         comida = 0.5;
         sonido = "miau miau";
-        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+        System.out.println("Mi " + especie + " se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
 
         nombre = "Manuelita";
         especie = "tortuga";
         edad = 12;
         comida = 0.3;
         sonido = "bloop bloop";
-        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+        System.out.println("Mi " + especie + " se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
 
         nombre = "Gardel";
         especie = "canario";
         edad = 1;
         comida = 0.35;
         sonido = "pio pio";
-        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+        System.out.println("Mi " + especie + " se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
     }
 }
