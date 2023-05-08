351f2a49eeaefa3a56964406b3be3ee2a08a997c
Andy <68301335+Lancito01@users.noreply.github.com>
2023-05-08 20:55:51 -0300

    actividad clase 1

---
+++ POO/actividad_clase_1.java
@@ -0,0 +1,38 @@
+public class actividad_clase_1{
+    public static void main(String[] args){
+        String nombre = "Manchitas";
+        String especie = "perro";
+        int edad = 2;
+        double comida = 1.5;
+        String sonido = "guau guau";
+        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+
+        nombre = "Nemo";
+        especie = "pez";
+        edad = 1;
+        comida = 0.14;
+        sonido = "glu glu";
+        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+
+        nombre = "Silvestre";
+        especie = "gato";
+        edad = 3;
+        comida = 0.5;
+        sonido = "miau miau";
+        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+
+        nombre = "Manuelita";
+        especie = "tortuga";
+        edad = 12;
+        comida = 0.3;
+        sonido = "bloop bloop";
+        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+
+        nombre = "Gardel";
+        especie = "canario";
+        edad = 1;
+        comida = 0.35;
+        sonido = "pio pio";
+        System.out.println("Mi " + especie + "se llama " + nombre + " tiene " + edad + " años, come " + comida + " kilos de comida y hace " + sonido);
+    }
+}
