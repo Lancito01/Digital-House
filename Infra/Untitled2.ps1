13dbb378c0135d7694c3c60d44295bf150c16506
Andy <68301335+Lancito01@users.noreply.github.com>
2023-08-08 00:40:27 -0300

    infra shell scripting

---
+++ Infra/Untitled2.ps1
@@ -0,0 +1,14 @@
+﻿$url = "http://ws.smn.gob.ar/map_items/weather"
+$localidad = Read-Host "Ingrese la localidad para la cual desea ver el clima"
+$response = Invoke-RestMethod -Uri $url -Method Get
+
+#Filtrar por localidad
+$climaLocalidad = $response | Where-Object { $_.name -eq $localidad }
+
+if($climaLocalidad) {
+    #Muestro info
+    Write-Host "Informacion del clima de la localidad $localidad"
+    Write-Host "Temperatura: $($climaLocalidad.weather.temp)"
+} else {
+    Write-Host "Lo siento, no encontramos la localidad $localidad"
+}
